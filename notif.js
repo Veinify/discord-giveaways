b8eb87e15adc0b2c15573fef4d107fd10ac4c14b
mirzabhakti <mirzabhakti@gmail.com>
2020-11-07 13:41:35 +0000

    yyy

---
+++ notif.js
@@ -0,0 +1,5 @@
+const { stripIndent } = require('common-tags');
+console.warn(stripIndent`
+  ******************************
+  This module is only specified for our project, if you still want to use it, you need to set things out yourself.
+  ******************************`)
